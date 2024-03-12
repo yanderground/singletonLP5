5e327dbb42c42d88142093824dd90618cd376c71
Yan <yanaugusto12@gmail.com>
2024-03-11 23:55:33 -0300

    tentando algo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,25 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Produto produto1 = Produto.getInstance("Camiseta", 29.99);
+        Produto produto2 = Produto.getInstance("Calça Jeans", 59.99);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+
+        System.out.println("Informações sobre o produto 1:");
+        produto1.exibirInformacoes();
+        System.out.println();
+
+        System.out.println("Informações sobre o produto 2:");
+        produto2.exibirInformacoes();
+        System.out.println();
+
+
+        produto1.setPreco(39.99);
+
+
+        System.out.println("Informações atualizadas sobre o produto 1:");
+        produto1.exibirInformacoes();
     }
 }
--- src/main/java/org/example/Produto.java
+++ src/main/java/org/example/Produto.java
@@ -1,2 +1,40 @@
-package org.example;public class Produto {
+package org.example;
+
+class Produto {
+    private static Produto instance = null;
+    private String nome;
+    private double preco;
+
+    private Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public static Produto getInstance(String nome, double preco) {
+        if (instance == null) {
+            instance = new Produto(nome, preco);
+        }
+        return instance;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public void exibirInformacoes() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Preço: R$" + preco);
+    }
 }
